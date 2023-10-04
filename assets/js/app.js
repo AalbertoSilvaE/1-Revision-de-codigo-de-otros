abe87f86a309d04385b9dc06f7ed8edfff572705
AlbertoSilvEn <aalbertosilvae@gmail.com>
2023-10-04 10:41:14 -0600

    Se resuelve el primer ejercicio de revision de  codigo de otros

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,33 @@
-const baseEndpoint = 'https://api.github.com';
-const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
+const baseEndpoint = 'https://api.github.com'; //API DE GITHUB
+const usersEndpoint = `${baseEndpoint}/users`; // USERS ES PARA 
+const n = document.querySelector('.name'); 
+const b = document.querySelector('.blog'); //SE CAMBIO EL 3 POR NUMERO POR QUE ES UNA CLASE NO UN ID 
+const l = document.querySelector('.location'); //ESTA CLASE SE CREO EN HTML
 
-function displayUser(username) {
-  $n.textContent = 'cargando...';
-  const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+
+async function displayUser(username) { //SE AGREGA async AL INICIO POR QUE EL AWAIT INIDICA QUE ES AINCRONA
+  n.textContent = 'cargando...'; 
+  const response = await fetch (`${usersEndpoint}/${username}`); //SE SOLICITA INFORMACION DEL USUARIO
+  const data = await response.json (); //SE UTILIZA PARA GUARDAR LA INFORMACION Y CONVERTIRLA A JSON
+
+  console.log(data); 
+  n.textContent = `${data.name}`;
+  b.textContent = `${data.blog}`;
+  l.textContent = `${data.location}`;
 }
 
-function handleError(err) {
+function handleError(err) { 
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  n.textContent = `Algo salió mal: ${err}` //SE CAMBIAN COMILLAS POR BACKTICKS
 }
 
-displayUser('stolinski').catch(handleError);
+displayUser('stolinski').catch(handleError); 
+
+
+
+
+
+//LA FUNCION DEL CODIGO ES SOLICITAR INFORMACION DE UN USUARIO DE GITHUB POR MEDIO D EUNA API, SI NO ES EXITOSA LA OBTENSION DE INFORMACION SE DESPLIEGAN ERRORES PARA PODER RESOLVER LO QUE TENGAMOS MAL.
+
+
